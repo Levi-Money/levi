41e10ddbdf1bb0ecdc38b66e91589f0fada4722f
Walker <walker@praiseweb.com.br>
2021-09-12 12:02:09 -0300

    feat(game): log world change

--- packages/game/src/Main.purs
+++ packages/game/src/Main.purs
@@ -5,12 +5,10 @@ import Prelude
 import Effect (Effect)
 import Engine.Console (log)
 import Engine.Web (requestFrame)
-import Game (genWorld, resume, readWorld)
+import Game (genWorld, readWorld, resume)
 
 main :: Effect Unit
 main = do 
   world <- genWorld
-  log "World started"
-  _ <- resume requestFrame world
-  worldState <- readWorld world
-  log $ "World updated: " <> show worldState
+  readWorld world >>= (\w -> log $ "World changed: " <> show w)
+  resume requestFrame world
