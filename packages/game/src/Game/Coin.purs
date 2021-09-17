829ca4dccb5eb2bb4fc32548753ef8c11f7156b1
Walker <walker@praiseweb.com.br>
2021-09-17 16:20:16 -0300

    refactor(game): transform x y into Point type

--- packages/game/src/Game/Coin.purs
+++ packages/game/src/Game/Coin.purs
@@ -5,42 +5,37 @@ import Prelude
 import Effect (Effect)
 import Record (merge)
 import Data.Array (last)
-import Data.Int (toNumber)
 import Data.Maybe (Maybe(..))
+import Math (pi)
 import Engine.Web as E
 import Game.Input (Touch) as I
-import Math (pi)
 
 type State = {
-      x :: Number
-    , y :: Number
+      position :: E.Point
     , enabled :: Boolean
 }
 
 initState :: Effect State
 initState = pure {
-      x: 50.0
-    , y: 50.0
+      position: E.makePoint 0 0
     , enabled: false
 }
 
 update :: Array I.Touch -> State -> State
 update touchs state
   | state.enabled = case last touchs of
-    Just touch -> merge {
-          x: toNumber touch.clientX
-        , y: toNumber touch.clientY
+    Just { clientX, clientY } -> merge {
+        position: E.makePoint clientX clientY
     } state
     Nothing -> state
   | otherwise = state
 
 render :: State -> E.State -> Effect Unit
-render {x, y, enabled } engineState
+render { position, enabled } engineState
   | enabled = do
     E.setColor engineState "#FFCF00"
     E.drawArc engineState {
-          x
-        , y
+          position
         , start: 0.0
         , end: 2.0 * pi
         , radius: 30.0
